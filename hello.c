8fb89f759ce9d168632548968c286e1ad1a2128c
ssunil1 <ssunil1@umbc.edu>
2025-10-30 20:21:23 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,6 +1,6 @@
 c#include <stdio.h>
 
 int main() {
-    printf("Hello teach. My name is Sumvritha and I'm in your CMPE310 class.\n");
+    printf("Hello teach. My name is Sumvritha and I'm in your CMPE310 class");
     return 0;
 }
